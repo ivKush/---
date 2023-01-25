aecffcf44e14cf0acfb5ef4336c9fa0354bfdf79
Иван Кушнир <kushnir82@mail.ru>
2023-01-25 10:10:23 +0300

    правка после проверки преподавателя

--- home_work_4/index.php
+++ home_work_4/index.php
@@ -122,18 +122,19 @@ $work_experience = [
                 <div class="w3-container w3-card w3-white w3-margin-bottom">
                     <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
                     <?php for ($i = 0; $i < count($work_experience); $i++) : ?>
-                        <?php if ($i == 0) : ?>
-                            <div class="w3-container">
+                        <div class="w3-container">
+                            <?php if ($i == 0) : ?>
                                 <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?= $work_experience[$i]["дата"]; ?>
-                                    <span class="w3-tag w3-teal w3-round">Current</span></h6>
+                                    <span class="w3-tag w3-teal w3-round">Current</span>
+                                </h6>
                             <?php else : ?>
                                 <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?= $work_experience[$i]["дата"]; ?></h6>
                             <?php endif ?>
                             <h5 class="w3-opacity"><b><?= $work_experience[$i]["должность"]; ?></b></h5>
                             <p><?= $work_experience[$i]["описание"] ?> </p>
-                        <?php endfor; ?>
-                        <hr>
-                            </div>
+                            <hr>
+                        </div>
+                    <?php endfor; ?>
                 </div>
                 <div class="w3-container w3-card w3-white">
                     <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
--- home_work_4/my.sql
+++ home_work_4/my.sql
@@ -3,26 +3,26 @@
 -- которые живут в Москве и их возраст находится в диапазоне [18, 30) лет.
 
 -- create
-CREATE TABLE AUDITORIUM (
-  Id INTEGER PRIMARY KEY,
+CREATE TABLE auditorium (
+  id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL,
   address TEXT NOT NULL
 );
 
 -- insert
-INSERT INTO AUDITORIUM VALUES (1, 'Вася', 23, 'Казань');
-INSERT INTO AUDITORIUM VALUES (2, 'Петя', 25, 'Томск');
-INSERT INTO AUDITORIUM VALUES (3, 'Света', 20, 'Питер');
-INSERT INTO AUDITORIUM VALUES (4, 'Лена', 18, 'Москва');
-INSERT INTO AUDITORIUM VALUES (5, 'Виталий', 30, 'Москва');
-INSERT INTO AUDITORIUM VALUES (6, 'Семён', 27, 'Екатеринбург');
-INSERT INTO AUDITORIUM VALUES (7, 'Наташа', 45, 'Москва');
-INSERT INTO AUDITORIUM VALUES (8, 'Иван', 40, 'Краснодар');
-INSERT INTO AUDITORIUM VALUES (9, 'Света', 35, 'Питер');
-INSERT INTO AUDITORIUM VALUES (10, 'Катя', 28, 'Москва');
+INSERT INTO auditorium VALUES (1, 'Вася', 23, 'Казань');
+INSERT INTO auditorium VALUES (2, 'Петя', 25, 'Томск');
+INSERT INTO auditorium VALUES (3, 'Света', 20, 'Питер');
+INSERT INTO auditorium VALUES (4, 'Лена', 18, 'Москва');
+INSERT INTO auditorium VALUES (5, 'Виталий', 30, 'Москва');
+INSERT INTO auditorium VALUES (6, 'Семён', 27, 'Екатеринбург');
+INSERT INTO auditorium VALUES (7, 'Наташа', 45, 'Москва');
+INSERT INTO auditorium VALUES (8, 'Иван', 40, 'Краснодар');
+INSERT INTO auditorium VALUES (9, 'Света', 35, 'Питер');
+INSERT INTO auditorium VALUES (10, 'Катя', 28, 'Москва');
 
 -- fetch 
-SELECT name FROM AUDITORIUM WHERE address = 'Москва' and age >= 18 and age <30 ;
+SELECT name FROM auditorium WHERE address = 'Москва' AND age >= 18 AND age <30;
 -- ссылка на решение в песочнице 
 -- https://onecompiler.com/mysql/3yvu33vqw
